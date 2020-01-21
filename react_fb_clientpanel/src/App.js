6d8e7432e359a65609869abecb8d8904bbff9230
Roopak Chugh <chughroopak@gmail.com>
2020-01-22 00:02:07 +0530

    update 1

---
+++ react_fb_clientpanel/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import AppNavBar from "./components/layouts/AppNavBar";
+import Dashboard from "./components/layouts/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store, { rrfProps } from "./store";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import AddClient from "./components/clients/AddClient";
+import ClientDetails from "./components/clients/ClientDetails";
+import EditClient from "./components/clients/EditClient";
+import Login from "./components/auth/Login";
+import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <Router>
+          <div className='App'>
+            <AppNavBar />
+            <div className='container'>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path='/client/add'
+                  component={UserIsAuthenticated(AddClient)}
+                />
+                <Route
+                  exact
+                  path='/client/:id'
+                  component={UserIsAuthenticated(ClientDetails)}
+                />
+                <Route
+                  exact
+                  path='/client/edit/:id'
+                  component={UserIsAuthenticated(EditClient)}
+                />
+                <Route
+                  exact
+                  path='/login'
+                  component={UserIsNotAuthenticated(Login)}
+                />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ react_fb_clientpanel/src/store.js
@@ -0,0 +1,50 @@
+import { createStore, combineReducers } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer, createFirestoreInstance } from "redux-firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDB1OfyUqfFQByQtjBgf6yUMp7rTZ5vFo8",
+  authDomain: "reactclientpanel-e62e8.firebaseapp.com",
+  databaseURL: "https://reactclientpanel-e62e8.firebaseio.com",
+  projectId: "reactclientpanel-e62e8",
+  storageBucket: "reactclientpanel-e62e8.appspot.com",
+  messagingSenderId: "622052870029",
+  appId: "1:622052870029:web:13ebda6c8fe044e5399fc4",
+  measurementId: "G-RFMSWENL58"
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
+
+//Init firebase instance
+firebase.initializeApp(firebaseConfig);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+//create initial state
+
+const initialState = {};
+
+//create store
+const store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+};
+
+export default store;
